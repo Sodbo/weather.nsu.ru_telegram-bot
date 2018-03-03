969d66a8e11269f7603d22fa8eaadf33241bbd84
Sodbo Sharapov <sharapovsodbo@gmail.com>
2018-03-03 20:20:44 +0700

    Splitted get_nsu_temp function into two

--- get_nsu_temp.py
+++ get_nsu_temp.py
@@ -3,6 +3,7 @@
 # MIT License
 
 import dryscrape
+import time
 from bs4 import BeautifulSoup
 
 def get_nsu_temp():
@@ -13,5 +14,10 @@ def get_nsu_temp():
     soup = BeautifulSoup(response, "lxml")
     nsu_temp = str(soup.find(id="temp"))
     nsu_temp = nsu_temp.split("<")[1].split('>')[1]
+    cur_time = time.time()
+    res = [nsu_temp , cur_time]
+    return(res)
+
+def message_nsu_temp(nsu_temp):
     weather_nsu_message = "The temperature nearby NSU is " + nsu_temp
     return(weather_nsu_message)
