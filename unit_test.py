4c482b732d5d558b5b96ccc20eaac97126447afb
Sodbo Sharapov <sharapovsodbo@gmail.com>
2018-03-03 20:19:58 +0700

    updated unit test

--- unit_test.py
+++ unit_test.py
@@ -1,13 +1,17 @@
 import unittest
 from get_nsu_temp import get_nsu_temp
+from get_nsu_temp import message_nsu_temp
 
 class TestStringMethods(unittest.TestCase):
 
     def test_get_nsu_temp_is_str(self):
-        self.assertTrue(isinstance(get_nsu_temp(), str))
+        self.assertTrue(isinstance(get_nsu_temp()[0], str))
+    
+    def test_get_nsu_temp_is_not_empty(self):
+        self.assertTrue(len(get_nsu_temp()[0])!=0)
 
-    def test_get_nsu_temp_has_5_words(self):
-        self.assertEqual(len(get_nsu_temp().split(' ')), 6)
+    def test_message_nsu_temp_has_6_words(self):
+        self.assertEqual(len(message_nsu_temp('-10C').split(' ')), 6)
 
 if __name__ == '__main__':
     unittest.main()
