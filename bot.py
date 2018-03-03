26f2980f7026c744df277741e51b83877db2e90b
Sodbo Sharapov <sharapovsodbo@gmail.com>
2018-03-03 14:45:45 +0700

    Bot can read string from the function get_nsu_temp written in the bot.py file

--- bot.py
+++ bot.py
@@ -1,17 +1,25 @@
 # Sodbo Sharapov
 # 20180303
-# GNU GPL2
+# MIT License
 
 # -*- coding: utf-8 -*-
 import telebot
 import config
+#import get_nsu_temp
 
+def get_nsu_temp():
+	weather_nsu_message = "The temrerature around NSU is 0 C"
+	return(weather_nsu_message)
+
+# Initialize bot
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 
+# Function for responding to the request
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, 'Doobs ne pidr')
+    bot.send_message(message.chat.id, get_nsu_temp())
 
+# Endless loop that keep bot running
 if __name__ == '__main__':
     bot.polling(none_stop=True)
