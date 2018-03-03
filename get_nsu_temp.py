8e37d9bcbe33203c81935bee484f99a511cb9384
Sodbo Sharapov <sharapovsodbo@gmail.com>
2018-03-03 16:43:17 +0700

    get_nsu_temp works correctly

--- get_nsu_temp.py
+++ get_nsu_temp.py
@@ -2,6 +2,18 @@
 # 20180303
 # MIT License
 
+import requests  # package for reading URLs
+import dryscrape
+from bs4 import BeautifulSoup
+import codecs
+
 def get_nsu_temp():
-    weather_nsu_message = 'The temrerature around NSU is 0 C'
+    wnsu_url = 'http://weather.nsu.ru/'
+    session = dryscrape.Session()
+    session.visit(wnsu_url)
+    response = session.body()
+    soup = BeautifulSoup(response, "lxml")
+    nsu_temp = str(soup.find(id="temp"))
+    nsu_temp = nsu_temp.split("<")[1].split('>')[1]
+    weather_nsu_message = "Weather around NSU is " + nsu_temp
     return(weather_nsu_message)
--- unit_test.py
+++ unit_test.py
@@ -3,8 +3,11 @@ from get_nsu_temp import get_nsu_temp
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_get_nsu_temp(self):
+    def test_get_nsu_temp_is_str(self):
         self.assertTrue(isinstance(get_nsu_temp(), str))
+
+    def test_get_nsu_temp_has_5_words(self):
+        self.assertEqual(len(get_nsu_temp().split(' ')), 5)
 
 if __name__ == '__main__':
     unittest.main()
